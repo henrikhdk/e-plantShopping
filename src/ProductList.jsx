040a0043c991f92db3da2ef6fb4c9aa840c9eec0
Henrik D'oark <henrikdoark@gmail.com>
2025-07-21 20:37:51 -0400

    Add reducer property of the slice for adding, removing, and updating

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { addItem } from "./CartSlice";
 import "./ProductList.css";
 import CartItem from "./CartItem";
 function ProductList({ onHomeClick }) {
   const [showCart, setShowCart] = useState(false);
   const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
   const [addedToCart, setAddedToCart] = useState({});
+  const dispatch = useDispatch();
 
   const handleAddToCart = (product) => {
     dispatch(addItem(product)); // Dispatch the action to add the product to the cart (Redux action)
